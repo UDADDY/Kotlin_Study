ef87a6d3a894bb1f1a3f9061049e7a8f7d8c21fa
UDADDY <gustmd5715@gmail.com>
2023-09-26 18:31:41 +0900

    Fist Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,46 @@
+import java.lang.IllegalArgumentException
+
+fun main() {
+    val person = Person("HI", -5)
+    print(person.name)
+}
+
+class Person(
+    val name: String,
+    var age: Int
+) {
+    // 생성자가 호출되는 시점
+    init {
+        if (age < 0) {
+            throw IllegalArgumentException("나이는 ${age}일 수 없습니다.")
+        }
+    }
+}
+
+fun max(a: Int, b: Int) = if (a > b) a else b
+
+fun printAll(vararg strings: String) {
+    for (str in strings) {
+        println(str)
+    }
+}
+
+fun validateScoreIsNotNegative(score: Int): Unit {
+    if (score < 0) {
+        throw IllegalArgumentException("${score}는 0보다 작을 수 없습니다.")
+    }
+}
+
+fun repeat(
+    str: String,
+    num: Int = 3,
+    userNewLine: Boolean = true
+) {
+    for (i in 1..num) {
+        if (userNewLine) {
+            println(str)
+        } else {
+            print(str)
+        }
+    }
+}
